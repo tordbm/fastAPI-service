8af092da9185fe649c36f618b7688eacf1145953
tordbm <114586011+tordbm@users.noreply.github.com>
2024-12-04 14:41:21 +0000

    update requirements and formatting

--- app/database.py
+++ app/database.py
@@ -1,9 +1,17 @@
 import databases
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 from .utils import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True, pool_pre_ping=True, pool_recycle=1800, pool_size=20, max_overflow=10)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    pool_size=20,
+    max_overflow=10,
+)
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
@@ -13,6 +21,7 @@ Base = declarative_base()
 database = databases.Database(DATABASE_URL)
 
 metadata = Base.metadata
+
 
 async def get_db():
     async with AsyncSessionLocal() as session:
