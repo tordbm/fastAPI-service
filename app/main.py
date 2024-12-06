a9c19f01aee26a71d8a605fc4c01ac330cee4f21
tordbm <tordbm@icloud.com>
2024-12-07 00:29:06 +0100

    Revert "Revert "update requirements and formatting""
    This reverts commit e2e1c1fed3edfe797f6f89fbc9301a8feb675235.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+from contextlib import asynccontextmanager
 from typing import Annotated, List
 from uuid import UUID
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.middleware.cors import CORSMiddleware
+
 import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import delete, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from . import models, schemas
-from .database import engine, get_db, database
-from contextlib import asynccontextmanager
-from .utils import verify_password, get_password_hash, create_access_token, SECRET_KEY, ALGORITHM, origins, oauth2_scheme
-from sqlalchemy import insert, select, delete
+from .database import database, engine, get_db
+from .utils import (
+    ALGORITHM,
+    SECRET_KEY,
+    create_access_token,
+    get_password_hash,
+    oauth2_scheme,
+    origins,
+    verify_password,
+)
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -18,6 +29,7 @@ async def lifespan(_: FastAPI):
         await conn.run_sync(models.metadata.create_all)
     yield
     await database.disconnect()
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -29,6 +41,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 async def authenticate_user(username: str, password: str, db: AsyncSession):
     user = await get_user_by_username(username, db)
     if not user:
@@ -37,7 +50,10 @@ async def authenticate_user(username: str, password: str, db: AsyncSession):
         return False
     return user
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: AsyncSession = Depends(get_db)):
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db: AsyncSession = Depends(get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -54,16 +70,21 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: As
         raise credentials_exception
     return user
 
-def get_current_active_user(current_user: Annotated[schemas.User, Depends(get_current_user)]):
+
+def get_current_active_user(
+    current_user: Annotated[schemas.User, Depends(get_current_user)],
+):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
 
 # Endpoints
 @app.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db: AsyncSession = Depends(get_db)) -> schemas.Token:
+    db: AsyncSession = Depends(get_db),
+) -> schemas.Token:
     user = await authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
@@ -74,6 +95,7 @@ async def login_for_access_token(
     access_token = create_access_token(data={"sub": user.username})
     return schemas.Token(access_token=access_token, token_type="bearer")
 
+
 @app.get("/users/me/", response_model=schemas.UserResponse)
 async def read_users_me(
     current_user: Annotated[schemas.User, Depends(get_current_active_user)],
@@ -83,17 +105,21 @@ async def read_users_me(
     except Exception as e:
         raise HTTPException(500, detail=str(e))
 
+
 @app.get("/users/me/cities/", response_model=List[schemas.FavoriteCities])
 async def read_own_cities(
     current_user: Annotated[schemas.User, Depends(get_current_active_user)],
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_db),
 ):
-    query = models.favored_cities.select().where(current_user.id == models.favored_cities.c.id)
+    query = models.favored_cities.select().where(
+        current_user.id == models.favored_cities.c.id
+    )
     result = await db.execute(query)
     cities = result.fetchall()
     if not cities:
         raise HTTPException(404, detail="No cities found")
     return cities
+
 
 @app.get("/allusers/", response_model=List[schemas.UserResponse])
 async def read_users(db: AsyncSession = Depends(get_db)):
@@ -104,6 +130,7 @@ async def read_users(db: AsyncSession = Depends(get_db)):
         raise HTTPException(404, detail="No users found")
     return users
 
+
 @app.post("/get_user_by_username/", response_model=schemas.UserResponse)
 async def get_user_by_username(username: str, db: AsyncSession = Depends(get_db)):
     query = models.users.select().where(models.users.c.username == username)
@@ -112,6 +139,7 @@ async def get_user_by_username(username: str, db: AsyncSession = Depends(get_db)
     if user is None:
         raise HTTPException(404, detail="User not found")
     return user
+
 
 @app.post("/get_user_by_id/", response_model=schemas.UserResponse)
 async def get_user_by_id(User: schemas.UserById, db: AsyncSession = Depends(get_db)):
@@ -122,55 +150,79 @@ async def get_user_by_id(User: schemas.UserById, db: AsyncSession = Depends(get_
         raise HTTPException(404, detail="User not found")
     return user
 
+
 @app.post("/add_favorite_city/", response_model=schemas.UserFavoredCityResponse)
-async def add_favorite_city(request: schemas.UserAddFavoriteCity,
-                            current_user: Annotated[schemas.User, Depends(get_current_active_user)],
-                            db: AsyncSession = Depends(get_db)):
+async def add_favorite_city(
+    request: schemas.UserAddFavoriteCity,
+    current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+    db: AsyncSession = Depends(get_db),
+):
     try:
-        query = insert(models.favored_cities).values(id=current_user.id, city=request.city)
+        query = insert(models.favored_cities).values(
+            id=current_user.id, city=request.city
+        )
         result = await db.execute(query)
         await db.commit()
-        return {"favored_id": result.inserted_primary_key[0], "username": current_user.username, "city": request.city}
+        return {
+            "favored_id": result.inserted_primary_key[0],
+            "username": current_user.username,
+            "city": request.city,
+        }
     except Exception as e:
         await db.rollback()
         raise HTTPException(400, detail="Could not add city: " + str(e))
 
+
 @app.post("/create_user/")
 async def create_user(request: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
     try:
-        query = insert(models.users).values(username=request.username, email=request.email, hashed_password=get_password_hash(request.password))
+        query = insert(models.users).values(
+            username=request.username,
+            email=request.email,
+            hashed_password=get_password_hash(request.password),
+        )
         result = await db.execute(query)
         await db.commit()
-        return {"id": result.inserted_primary_key[0], "username": request.username, "email": request.email}
+        return {
+            "id": result.inserted_primary_key[0],
+            "username": request.username,
+            "email": request.email,
+        }
     except Exception as e:
         await db.rollback()
         raise HTTPException(400, detail="User already exists: " + str(e))
 
+
 @app.delete("/delete_favored_city/")
-async def delete_favored_city(favored_id: UUID,
-                              _: Annotated[schemas.User, Depends(get_current_active_user)],
-                              db: AsyncSession = Depends(get_db)
-    ):
+async def delete_favored_city(
+    favored_id: UUID,
+    _: Annotated[schemas.User, Depends(get_current_active_user)],
+    db: AsyncSession = Depends(get_db),
+):
     try:
-        query = delete(models.favored_cities).where(models.favored_cities.c.favored_id == favored_id)
+        query = delete(models.favored_cities).where(
+            models.favored_cities.c.favored_id == favored_id
+        )
         result = await db.execute(query)
         if result.rowcount == 0:
-             raise HTTPException(status_code=404, detail="City not found")
+            raise HTTPException(status_code=404, detail="City not found")
         await db.commit()
     except Exception as e:
         await db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.delete("/users/delete_user/")
-async def delete_user(id: UUID,
-                              _: Annotated[schemas.User, Depends(get_current_active_user)],
-                              db: AsyncSession = Depends(get_db)
-    ):
+async def delete_user(
+    id: UUID,
+    _: Annotated[schemas.User, Depends(get_current_active_user)],
+    db: AsyncSession = Depends(get_db),
+):
     try:
         query = delete(models.users).where(models.users.c.id == id)
         result = await db.execute(query)
         if result.rowcount == 0:
-             raise HTTPException(status_code=404, detail="User not found")
+            raise HTTPException(status_code=404, detail="User not found")
         await db.commit()
     except Exception as e:
         await db.rollback()
--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,14 @@
 import os
-from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
-from fastapi.security import OAuth2PasswordBearer
+
 import jwt
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = os.getenv('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 origins = [
     "http://localhost:5173",
     "https://tordbm.github.io",
@@ -17,11 +16,14 @@ origins = [
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
